06b2067fb509067c739e1f7a53a38300b0c03f5f
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2026-03-10 14:14:53 +0100

    Added buttons to UI, styling and a script

---
+++ server/public/scripts.js
@@ -0,0 +1,5 @@
+    const apiTypeSelect = document.getElementById("apiType");
+    const getAllBtn = document.getElementById("getAllBtn");
+    const getOneBtn = document.getElementById("getOneBtn");
+    const rowIdInput = document.getElementById("rowId");
+    const output = document.getElementById("output");
