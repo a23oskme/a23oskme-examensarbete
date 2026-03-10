c65ab99e52ee42482c51134b608cdf155717b0e6
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2026-03-10 18:48:35 +0100

    Missed to commit indentation fix

--- server/public/scripts.js
+++ server/public/scripts.js
@@ -1,8 +1,3 @@
-    const apiTypeSelect = document.getElementById("apiType");
-    const getAllBtn = document.getElementById("getAllBtn");
-    const getOneBtn = document.getElementById("getOneBtn");
-    const rowIdInput = document.getElementById("rowId");
-    const output = document.getElementById("output");
 const apiTypeSelect = document.getElementById("apiType");
 const getAllBtn = document.getElementById("getAllBtn");
 const getOneBtn = document.getElementById("getOneBtn");
