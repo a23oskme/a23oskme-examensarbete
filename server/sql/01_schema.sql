0fb9e6aaf379285ff8c40bbe803e49ede613b350
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2026-04-04 09:57:36 +0200

    Replaced test tables with the real ones

--- server/sql/01_schema.sql
+++ server/sql/01_schema.sql
@@ -1,4 +1,33 @@
-create table test_table (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+CREATE TABLE pages (
+    page_id BIGINT PRIMARY KEY,
+    page_namespace INTEGER,
+    page_title TEXT,
+    page_is_redirect INTEGER
+);
+
+CREATE TABLE linktarget (
+    lt_id BIGINT PRIMARY KEY,
+    lt_namespace INTEGER,
+    lt_title TEXT
+);
+
+CREATE TABLE category (
+    cat_id BIGINT PRIMARY KEY,
+    cat_title TEXT
+);
+
+CREATE TABLE pagelinks (
+    pl_from BIGINT,
+    pl_from_namespace INTEGER,
+    pl_target_id BIGINT,
+    PRIMARY KEY (pl_from, pl_target_id),
+    FOREIGN KEY (pl_from) REFERENCES pages(page_id),
+    FOREIGN KEY (pl_target_id) REFERENCES linktarget(lt_id)
+);
+
+CREATE TABLE categorylinks (
+    cl_from BIGINT,
+    cl_to BIGINT,
+    PRIMARY KEY (cl_from, cl_to),
+    FOREIGN KEY (cl_from) REFERENCES pages(page_id)
 );
