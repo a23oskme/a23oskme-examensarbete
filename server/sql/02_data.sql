afaea17bcb1419181af569855301c6fd7ad63cd6
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2026-04-07 09:38:06 +0200

    Added imports to data script that imports the data from .tsv-files

--- server/sql/02_data.sql
+++ server/sql/02_data.sql
@@ -1,1 +1,34 @@
-INSERT INTO test_table(name) VALUES ('test1'), ('test2'), ('test3');
+COPY pages (page_id, page_namespace, page_title, page_is_redirect)
+FROM '/docker-entrypoint-initdb.d/pages.tsv'
+WITH (
+    FORMAT text,
+    DELIMITER E'\t'
+);
+
+COPY linktarget (lt_id, lt_namespace, lt_title)
+FROM '/docker-entrypoint-initdb.d/linktarget.tsv'
+WITH (
+    FORMAT text,
+    DELIMITER E'\t'
+);
+
+COPY category (cat_id, cat_title)
+FROM '/docker-entrypoint-initdb.d/category.tsv'
+WITH (
+    FORMAT text,
+    DELIMITER E'\t'
+);
+
+COPY pagelinks (pl_from, pl_from_namespace, pl_target_id)
+FROM '/docker-entrypoint-initdb.d/pagelinks.tsv'
+WITH (
+    FORMAT text,
+    DELIMITER E'\t'
+);
+
+COPY categorylinks (cl_from, cl_to)
+FROM '/docker-entrypoint-initdb.d/categorylinks.tsv'
+WITH (
+    FORMAT text,
+    DELIMITER E'\t'
+);
