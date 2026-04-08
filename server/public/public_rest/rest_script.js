9f14956cf8a22852d2b367efedbd4cfffbc5513f
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2026-04-08 13:36:44 +0200

    Removed old (test)REST-functions in the script

--- server/public/public_rest/rest_script.js
+++ server/public/public_rest/rest_script.js
@@ -1,6 +1,3 @@
-const getAllBtn = document.getElementById("getAllBtn");
-const getOneBtn = document.getElementById("getOneBtn");
-const rowIdInput = document.getElementById("rowId");
 const output = document.getElementById("output");
 
 function showResult(data) {
@@ -12,53 +9,21 @@ function showError(error) {
   output.textContent = "Error:\n" + error;
 }
 
-// Function that fetches all rows from the chosen API
-async function fetchAll() {
   try {
-    // Variable to hold the response
-    let responseData;
-    // Send a GET-request to the REST-endpoint to get all rows
-    const response = await fetch("/rest/test-table");
-    // If the response is not okey, throw an error
-    if (!response.ok) {
-      throw new Error(`REST-fetch failed: ${response.status}`);
-    } else {
-      // If else, the response is okey, turn the JSON-response into a JavaScrip-object
-      responseData = await response.json();
-    }
-    
-    showResult(responseData);
   } catch (error) {
     showError(error.message);
   }
 }
 
-// Function that fetches one row based on id
-async function fetchOne() {
-  // Get inserted id
-  const id = rowIdInput.value;
 
-  // Show error if the user did not enter an id
-  if (!id) {
-    showError("Enter ID.");
-    return;
   }
 
   try {
-    let responseData;
 
-    // Fetch from REST-endpoint for a specific row
-    const response = await fetch(`/rest/test-table/${id}`);
-    if (!response.ok) {
-      throw new Error(`REST-fetch failed: ${response.status}`);
     }
-    responseData = await response.json();
 
-    showResult(responseData);
   } catch (error) {
     showError(error.message);
   }
 }
 
-getAllBtn.addEventListener("click", fetchAll);
-getOneBtn.addEventListener("click", fetchOne);
