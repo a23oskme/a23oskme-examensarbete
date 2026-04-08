c5a9ae22953138a668e80582b585602892e9aa26
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2026-04-08 18:41:08 +0200

    Created a generic reusable function that fetches from the REST API, and a function that uses it to fetch a page

--- server/public/public_rest/rest_script.js
+++ server/public/public_rest/rest_script.js
@@ -1,4 +1,5 @@
 const output = document.getElementById("output");
+const timingOutput = document.getElementById("timing");
 
 function showResult(data) {
   // Change JavaScript-object into formatted JSON-string
@@ -7,6 +8,34 @@ function showResult(data) {
 
 function showError(error) {
   output.textContent = "Error:\n" + error;
+}
+
+function showTiming(ms) {
+  timingOutput.textContent = `Request time: ${ms.toFixed(2)} ms`;
+}
+
+async function fetchRestData(url) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP ${response.status}`);
+  }
+
+  return await response.json();
+}
+
+// GET ONE PAGE
+async function fetchPageById(pageId) {
+  try {
+    const start = performance.now();
+    const data = await fetchRestData(`/rest/pages/${pageId}`);
+    const end = performance.now();
+
+    showTiming(end - start);
+    showResult(data);
+  } catch (error) {
+    showError(error.message);
+  }
 }
 
   try {
@@ -26,4 +55,16 @@ function showError(error) {
     showError(error.message);
   }
 }
+
+// GET HTML ELEMENTS AND APPLY EVENTLISTENERS
+document.getElementById("getPageBtn").addEventListener("click", () => {
+  const pageId = document.getElementById("pageIdInput").value.trim();
+
+  if (!pageId) {
+    showError("Please enter a page id.");
+    return;
+  }
+
+  fetchPageById(pageId);
+});
 
