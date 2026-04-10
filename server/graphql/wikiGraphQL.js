f9a59fcb583d4f9ec5ad77d7aae0af2efa3d772e
Oskar Steise <164857506+a23oskme@users.noreply.github.com>
2026-04-10 08:44:25 +0200

    Created the new GraphQL schema

--- server/graphql/wikiGraphQL.js
+++ server/graphql/wikiGraphQL.js
@@ -2,4 +2,24 @@ import { buildSchema } from "graphql";
 import { createHandler } from "graphql-http/lib/use/express";
 import pool from "../db.js";
 
+const schema = buildSchema(`
+  type Page {
+    page_id: ID!
+    page_namespace: Int
+    page_title: String
+    page_is_redirect: Int
+    categories: [Category!]!
+  }
+
+  type Category {
+    cat_id: ID!
+    cat_title: String
+    pages: [Page!]!
+  }
+
+  type Query {
+    page(id: ID!): Page
+    category(id: ID!): Category
+  }
+`);
 export default wikiGraphqlHandler;
